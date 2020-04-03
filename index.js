da9ad37456433533091ec018889a83a073d4c6cf
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-03 23:09:44 +0430

    overview page is done :)

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+const replaceTemplate = (template, data) => {
+    let output = template
+        .replace(/{%PRODUCT_NAME%}/g, data.productName)
+        .replace(/{%PRODUCT_IMAGE%}/g, data.image)
+        .replace(/{%PRODUCT_PRICE%}/g, data.price)
+        .replace(/{%PRODUCT_FROM%}/g, data.from)
+        .replace(/{%PRODUCT_HEALTH%}/g, data.nutrients)
+        .replace(/{%PRODUCT_QUANTITY%}/g, data.quantity)
+        .replace(/{%PRODUCT_DESCRIPTION%}/g, data.description)
+        .replace(/{%PRODUCT_ID%}/g, data.id);
+
+    if (!data.organic) {
+        output = output.replace(/{%PRODUCT_NOT_ORGANIC%}/g, 'not-organic');
+    }
+
+    return output;
+};
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const productsData = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const parsedData = JSON.parse(productsData);
 
 const server = http.createServer((req, res) => {
     const path = req.url;
@@ -11,7 +34,9 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
             'Content-type': 'text/html'
         });
-        res.end('<h1>Home Page</h1>')
+        const cardsHTML = parsedData.map(product => replaceTemplate(tempCard, product)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+        res.end(output);
     } else if (path === '/product') {
         res.writeHead(200, {
             'Content-type': 'text/html'
